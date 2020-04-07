2381c601d80415947031f9dace9f56559d8c339d
Rob Mecham <stemmetje@gmail.com>
2020-04-07 10:43:09 -0600

    Changed the build versioning system. Used a file to specify DOMjudge version. Added docker push to the build script.

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,50 +1,51 @@
 #!/bin/bash -e
 
-VERSION=$1
-if [[ -z ${VERSION} ]]
-then
-    echo "Usage: $0 domjudge-version"
-    echo "	For example: $0 5.3.0"
-    exit 1
+# Establish build number.
+LAST_BUILD=$(<.lastbuild)
+TIMESTAMP="$(date "+%Y%m")"
+if [ "${LAST_BUILD:0:6}" == "$TIMESTAMP" ]; then
+    next_build="$((${LAST_BUILD:7:15} + 1))"
+    BUILD="${TIMESTAMP}.${next_build}"
+else
+    BUILD="${TIMESTAMP}.1"
 fi
+echo "[..] Initiating build ${BUILD}."
 
-URL=https://www.domjudge.org/releases/domjudge-${VERSION}.tar.gz
+# Download DOMjudge package.
+DJ_VERSION=$(<.djversion)
+URL=https://www.domjudge.org/releases/domjudge-${DJ_VERSION}.tar.gz
 FILE=domjudge.tar.gz
-
-echo "[..] Downloading DOMJuge version ${VERSION}..."
-
+echo "[..] Downloading DOMJuge version ${DJ_VERSION}..."
 if ! curl -f -s -o ${FILE} ${URL}
 then
-    echo "[!!] DOMjudge version ${VERSION} file not found on https://www.domjudge.org/releases"
+    echo "[!!] DOMjudge version ${DJ_VERSION} file not found on https://www.domjudge.org/releases"
     exit 1
 fi
-
-echo "[ok] DOMjudge version ${VERSION} downloaded as domjudge.tar.gz"; echo
+echo "[ok] DOMjudge version ${DJ_VERSION} downloaded as domjudge.tar.gz"; echo
 
 echo "[..] Building Docker image for domserver using intermediate build image..."
-docker build -t tsacoding/domserver:${VERSION} -f domserver/Dockerfile .
+docker build -t tsacoding/domserver:${BUILD} -t tsacoding/domserver:latest -f domserver/Dockerfile .
 echo "[ok] Done building Docker image for domserver"
 
 echo "[..] Building Docker image for judgehost using intermediate build image..."
-docker build -t tsacoding/judgehost:${VERSION}-build -f judgehost/build.Dockerfile .
-docker rm -f tsacoding-judgehost-${VERSION}-build > /dev/null 2>&1 || true
-docker run -it --name tsacoding-judgehost-${VERSION}-build --privileged tsacoding/judgehost:${VERSION}-build
-docker cp tsacoding-judgehost-${VERSION}-build:/chroot.tar.gz .
-docker cp tsacoding-judgehost-${VERSION}-build:/judgehost.tar.gz .
-docker rm -f tsacoding-judgehost-${VERSION}-build
-docker rmi tsacoding/judgehost:${VERSION}-build
-docker build -t tsacoding/judgehost:${VERSION} -f judgehost/Dockerfile .
+docker build -t tsacoding/judgehost:${BUILD}-build -f judgehost/build.Dockerfile .
+docker rm -f tsacoding-judgehost-${BUILD}-build > /dev/null 2>&1 || true
+docker run -it --name tsacoding-judgehost-${BUILD}-build --privileged tsacoding/judgehost:${BUILD}-build
+docker cp tsacoding-judgehost-${BUILD}-build:/chroot.tar.gz .
+docker cp tsacoding-judgehost-${BUILD}-build:/judgehost.tar.gz .
+docker rm -f tsacoding-judgehost-${BUILD}-build
+docker rmi tsacoding/judgehost:${BUILD}-build
+docker build -t tsacoding/judgehost:${BUILD} -t tsacoding/judgehost:latest -f judgehost/Dockerfile .
 echo "[ok] Done building Docker image for judgehost"
 
-echo "[..] Building Docker image for judgehost chroot..."
-# docker build -t tsacoding/default-judgehost-chroot:${VERSION} -f judgehost/Dockerfile.chroot .
-echo "[ok] Done building Docker image for judgehost chroot"
-
-echo "All done. Image tsacoding/domserver:${VERSION} and tsacoding/judgehost:${VERSION} created"
-echo "If you are a DOMjudge maintainer with access to the tsacoding organization on Docker Hub, you can now run the following command to push them to Docker Hub:"
-echo "$ docker push tsacoding/domserver:${VERSION} && docker push tsacoding/judgehost:${VERSION} && docker push tsacoding/default-judgehost-chroot:${VERSION}"
-echo "If this is the latest release, also run the following command:"
-echo "$ docker tag tsacoding/domserver:${VERSION} tsacoding/domserver:latest && \
-docker tag tsacoding/judgehost:${VERSION} tsacoding/judgehost:latest && \
-docker tag tsacoding/default-judgehost-chroot:${VERSION} tsacoding/default-judgehost-chroot:latest && \
-docker push tsacoding/domserver:latest && docker push tsacoding/judgehost:latest && docker push tsacoding/default-judgehost-chroot:latest"
+read -p "Accept these builds and push to Docker Hub? " -n 1 -r
+echo  # Advance a line.
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    # Write out the build ID.
+    echo "$BUILD" > .lastbuild
+    # Push the images to Docker Hub.
+    docker push tsacoding/domserver:${BUILD}
+    docker push tsacoding/judgehost:${BUILD}
+    docker push tsacoding/domserver:latest
+    docker push tsacoding/judgehost:latest
+fi
