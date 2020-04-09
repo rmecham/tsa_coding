a11eae448d856ebf27cbf9bfbf74a64db2fc244d
Rob Mecham <stemmetje@gmail.com>
2020-04-08 23:10:05 -0600

    Enabled support for C#.

--- Docker/judgehost/chroot-and-tar.sh
+++ Docker/judgehost/chroot-and-tar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/opt/domjudge/judgehost/bin/dj_make_chroot -i ruby,nodejs
+/opt/domjudge/judgehost/bin/dj_make_chroot -i ruby,nodejs,mono-devel
 
 cd /
 tar -czvpf /chroot.tar.gz /chroot
